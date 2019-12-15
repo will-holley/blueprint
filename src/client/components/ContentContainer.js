f8e693dd39300d7ebb94162bc8588e4a698ed0e9
William A. Holley IV <will@vivism.com>
2019-12-14 20:04:53 -0500

    Moves containers to their own dir

--- src/client/components/ContentContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { Flex } from '@rebass/grid';
-
-const ContentContainer = styled(Flex).attrs({})``;
-
-export default ContentContainer;
