f4f9be7603ff8ce3962962298cfa564f37be1318
William A. Holley IV <will@vivism.com>
2019-12-18 12:10:31 -0500

    Adds inter as default font

--- src/client/config/globalStyles.js
+++ src/client/config/globalStyles.js
@@ -1,6 +1,43 @@
 import { createGlobalStyle } from "styled-components";
 
+// Import fonts
+// TODO: clean all this up!
+import InterRegularWoffTwo from "./../static/fonts/inter/Inter-Regular.woff2";
+import InterRegularWoff from "./../static/fonts/inter/Inter-Regular.woff";
+import InterMediumWoffTwo from "./../static/fonts/inter/Inter-Medium.woff2";
+import InterMediumWoff from "./../static/fonts/inter/Inter-Medium.woff";
+import InterBoldWoffTwo from "./../static/fonts/inter/Inter-Bold.woff2";
+import InterBoldWoff from "./../static/fonts/inter/Inter-Bold.woff";
+
 const GlobalStyle = createGlobalStyle`
+
+	// Setup font faces
+	@font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: normal;
+    src:
+      url('${InterRegularWoffTwo}') format('woff2'),
+      url('${InterRegularWoff}') format('woff');
+	}
+	@font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    src:
+      url('${InterMediumWoffTwo}') format('woff2'),
+      url('${InterMediumWoff}') format('woff');
+	}
+	@font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: bold;
+    src:
+      url('${InterBoldWoffTwo}') format('woff2'),
+      url('${InterBoldWoff}') format('woff');
+	}
+
+	// Declarations
   html {
   	font-size: 62.5%;
   	box-sizing: border-box;
@@ -15,7 +52,7 @@ const GlobalStyle = createGlobalStyle`
   	-moz-osx-font-smoothing: grayscale;
 	}
 	body {
-		font-family: sans-serif;
+		font-family: 'Inter', sans-serif;
 		font-size: 1.6rem;
 		min-height: 100vh;
 		// Project Defaults ^
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,14 @@ module.exports = {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: [
+          {
+            loader: "url-loader"
+          }
+        ]
       }
     ]
   },
