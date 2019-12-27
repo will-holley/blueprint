604706a4a930d6a40746139944a9947862c01c4a
William A. Holley IV <will@vivism.com>
2019-12-27 17:49:16 -0500

    Adds colored h1

---
+++ src/client/components/ColoredH1.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+// Components
+import { H1 } from "client/components/tags";
+// Styles
+import { randomGradient } from "client/config/gradients";
+
+const ColoredH1 = styled(H1)`
+  background: ${randomGradient};
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  &:hover {
+    // color: ${({ theme }) => theme.color};
+    // -webkit-text-fill-color: unset;
+    ${({ interactive }) =>
+      interactive &&
+      `
+      opacity: 0.7;
+    `}
+  }
+`;
+
+ColoredH1.propTypes = {
+  interactive: PropTypes.bool.isRequired
+};
+
+ColoredH1.defaultProps = {
+  interactive: false
+};
+
+export default ColoredH1;
