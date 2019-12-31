df41de0a71208dfb69d05b53ed618dc36299113d
William A. Holley IV <will@vivism.com>
2019-12-30 23:45:57 -0500

    Adds user actions

---
+++ src/client/data/actions/user.js
@@ -0,0 +1,8 @@
+import update from "immutability-helper";
+
+const actions = {
+  login: (email, password) => async ({ getState, setState }) => {},
+  join: (name, email, password) => async ({ getState, setState }) => {}
+};
+
+export default actions;
--- src/client/data/store.js
+++ src/client/data/store.js
@@ -9,11 +9,12 @@ import initialState from "./state";
 // Actions
 import { default as docActions } from "./actions/document";
 import { default as uiActions } from "./actions/ui";
+import { default as userActions } from "./actions/user";
 
 // Config
 defaults.devtools = process.env.NODE_ENV == "development";
 
-const actions = [docActions, uiActions];
+const actions = [docActions, uiActions, userActions];
 
 const store = createStore({
   initialState,
