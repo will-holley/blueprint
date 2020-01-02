975b9ea765410fb2b894f0f34df2ffdf9c349ca2
William A. Holley IV <will@vivism.com>
2020-01-02 12:48:24 -0500

    adds `created_by` and `private` fields to document model

--- src/server/data/models/document.js
+++ src/server/data/models/document.js
@@ -1,11 +1,21 @@
 import Model from "./_model";
+import User from "./user";
 
 class Document extends Model {
   static get table() {
     return "document";
   }
   static _addFields(table) {
+    //? Name
     table.string("name").nullable();
+    //? Owner Foreign key
+    table.uuid("created_by").notNullable();
+    table.foreign("created_by").references(`${User.table}.id`);
+    //? Visibility
+    table
+      .boolean("private")
+      .notNullable()
+      .defaultTo(true);
   }
 }
 
