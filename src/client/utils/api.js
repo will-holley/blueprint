a6a96b1b26e7bed58b76a893bbfc00c09a1272fc
William A. Holley IV <will@vivism.com>
2020-01-02 14:37:49 -0500

    cleans up API singleton

--- src/client/utils/api.js
+++ src/client/utils/api.js
@@ -3,33 +3,38 @@ import axios from "axios";
 class APISingleton {
   //! Statics
   tokenName = "jwt";
+  client = axios.create({
+    baseURL: `${process.env.API_ADDRESS}/api/1/`,
+    timeout: 1000,
+    headers: {
+      Authorization: `Bearer ${this.token}`
+    }
+  });
 
-  constructor() {
-    this.client = axios.create({
-      baseURL: `${process.env.API_ADDRESS}/api/1/`,
-      timeout: 1000,
-      headers: {
-        //$ On application load, if a token exists, use it to authenticate
-        //$ requests.
-        Authorization: `Bearer ${this.jwt}`
-      }
-    });
-  }
-
-  get jwt() {
+  get token() {
     const token = window.localStorage.getItem(this.tokenName);
     return token ? token : "";
   }
 
   get isAuthenticated() {
-    return Boolean(this.jwt);
+    return Boolean(this.token);
   }
 
+  /**
+   * Adds token to localStorage so that it persists between client sessions
+   * and, as such, indicates to the API that the user is still logged in.  Token
+   * is also added to the Authorization header of the axios client in order to
+   * authenticate all requests for the remainder of this session.
+   * @param {string} token A JSON Web Token for the current user.
+   */
   authenticate(token) {
     window.localStorage.setItem(this.tokenName, token);
     this.updateAuthorizationHeader(token);
   }
 
+  /**
+   * Removes JWT from localStorage and Authorization header.
+   */
   deAuthenticate() {
     //$ Erase session demarcation.
     window.localStorage.removeItem(this.tokenName);
@@ -37,6 +42,10 @@ class APISingleton {
     this.updateAuthorizationHeader("");
   }
 
+  /**
+   * Sets the axios client's Authorization header.
+   * @param {string} A JWT or empty string.
+   */
   updateAuthorizationHeader(token) {
     this.client.defaults.headers["Authorization"] = `Bearer ${token}`;
   }
