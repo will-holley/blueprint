d22284ac86b6590c4bdf7d443594e9b4ab06e19f
William A. Holley IV <will@vivism.com>
2020-01-02 13:58:15 -0500

    protect  private document editing endpoints for owner-only changes

--- src/server/api/document.js
+++ src/server/api/document.js
@@ -12,10 +12,7 @@ import { requiresAuthentication } from "./middleware";
 //! Create Router
 const router = express();
 
-//! ===============
-//! == Endpoints ==
-//! ===============
-
+//! Fields returned by document selections
 const fields = [
   "id",
   "human_id as humanId",
@@ -24,6 +21,36 @@ const fields = [
   "created_by as createdBy",
   "private"
 ];
+
+//! ================
+//! == Middleware ==
+//! ================
+
+/**
+ * If this is a private document and the user does not match,
+ * reject this request.
+ */
+const requiresDocumentOwnership = async (
+  { params: { id }, user },
+  res,
+  next
+) => {
+  try {
+    const [doc] = await db(Document.table)
+      .select(["private", "created_by as createdBy"])
+      .where("id", id)
+      .returning(1);
+    return !doc || (doc.private && doc.createdBy !== user._id)
+      ? res.sendStatus(400)
+      : next();
+  } catch (error) {
+    return res.send({ error: error.message });
+  }
+};
+
+//! ===============
+//! == Endpoints ==
+//! ===============
 
 router.get("/", async function fetchAllDocuments(req, res) {
   try {
@@ -86,92 +113,98 @@ router.post("/", requiresAuthentication, async function createDocument(
 /**
  * Load the nodes and edges for a document.
  */
-router.get("/:id", async function fetchDocumentDetails(
-  { params: { id } },
-  res
-) {
-  //? Client expects nodes and edges as id value maps.
-  const details = {
-    nodes: {},
-    edges: {}
-  };
-  try {
-    //? Query Nodes
-    const nodes = await db(Node.table)
-      .select([
-        "id",
-        "human_id as humanId",
-        "content",
-        "content_type as contentType"
-      ])
-      .where({ document: id, deleted_at: null })
-      .orderBy("created_at");
-    //? Add to id value map with additional details. Denote the base node.
-    let baseMarked = false;
-    nodes.forEach(node => {
-      if (!baseMarked) {
-        node.isBase = true;
-        baseMarked = true;
-      }
-      details.nodes[node.id] = node;
-    });
-    //? Create a list of node ids for querying edges
-    const nodeIds = nodes.map(({ id }) => id);
-    //? Query Edges
-    const edges = await db(Edge.table)
-      .select([
-        "id",
-        "human_id as humanId",
-        "has_parent as hasParent",
-        "node_a as nodeA",
-        "node_b as nodeB"
-      ])
-      .whereIn("node_a", nodeIds)
-      .orWhereIn("node_b", nodeIds);
-    //? Map edges to object
-    edges.forEach(edge => {
-      details.edges[edge.id] = edge;
-    });
-    //? Return details to client
-    res.status(200);
-    res.send(details);
-  } catch (error) {
-    res.send(error.message);
+router.get(
+  "/:id",
+  requiresDocumentOwnership,
+  async function fetchDocumentDetails({ params: { id } }, res) {
+    //? Client expects nodes and edges as id value maps.
+    const details = {
+      nodes: {},
+      edges: {}
+    };
+    try {
+      //? Query Nodes
+      const nodes = await db(Node.table)
+        .select([
+          "id",
+          "human_id as humanId",
+          "content",
+          "content_type as contentType"
+        ])
+        .where({ document: id, deleted_at: null })
+        .orderBy("created_at");
+      //? Add to id value map with additional details. Denote the base node.
+      let baseMarked = false;
+      nodes.forEach(node => {
+        if (!baseMarked) {
+          node.isBase = true;
+          baseMarked = true;
+        }
+        details.nodes[node.id] = node;
+      });
+      //? Create a list of node ids for querying edges
+      const nodeIds = nodes.map(({ id }) => id);
+      //? Query Edges
+      const edges = await db(Edge.table)
+        .select([
+          "id",
+          "human_id as humanId",
+          "has_parent as hasParent",
+          "node_a as nodeA",
+          "node_b as nodeB"
+        ])
+        .whereIn("node_a", nodeIds)
+        .orWhereIn("node_b", nodeIds);
+      //? Map edges to object
+      edges.forEach(edge => {
+        details.edges[edge.id] = edge;
+      });
+      //? Return details to client
+      res.status(200);
+      res.send(details);
+    } catch (error) {
+      res.send(error.message);
+    }
   }
-});
+);
 
-router.put("/:id", requiresAuthentication, async function updateDocumentDetails(
-  { params: { id }, body: { name } },
-  res
-) {
-  //? Compose update object with allowable fields
-  const fields = {};
-  if (name) fields["name"] = name;
-  //? Update
-  try {
-    await db(Document.table)
-      .where("id", id)
-      .update(fields);
-    res.sendStatus(200);
-  } catch (error) {
-    res.send(error.message);
+router.put(
+  "/:id",
+  [requiresAuthentication, requiresDocumentOwnership],
+  async function updateDocumentDetails(
+    { params: { id }, body: { name } },
+    res
+  ) {
+    //? Compose update object with allowable fields
+    const fields = {};
+    if (name) fields["name"] = name;
+    //? Update
+    try {
+      await db(Document.table)
+        .where("id", id)
+        .update(fields);
+      res.sendStatus(200);
+    } catch (error) {
+      res.send(error.message);
+    }
   }
-});
+);
 
-router.delete("/:id", requiresAuthentication, async function deleteDocument(
-  { params: { id } },
-  res
-) {
-  try {
-    const updatedCount = await db(Document.table)
-      .where("id", id)
-      .update({ deleted_at: new Date() });
-    // Response will indicate how many records were updated.
-    const responseStatus = updatedCount !== 0 ? 200 : 400;
-    res.sendStatus(responseStatus);
-  } catch (error) {
-    res.send(error.message);
+router.delete(
+  "/:id",
+  [requiresAuthentication, requiresDocumentOwnership],
+  async function deleteDocument({ params: { id } }, res) {
+    try {
+      const updatedCount = await db(Document.table)
+        .where("id", id)
+        .update({ deleted_at: new Date() });
+      // Response will indicate how many records were updated.
+      const responseStatus = updatedCount !== 0 ? 200 : 400;
+      res.sendStatus(responseStatus);
+    } catch (error) {
+      res.send(error.message);
+    }
   }
-});
+);
 
 export default router;
