dbc61cdbccf40a1db2732f94d740c5fa33e85a8e
William A. Holley IV <will@vivism.com>
2020-01-07 09:52:15 -0500

    fixes jwt middleware

--- src/server/api/middleware.js
+++ src/server/api/middleware.js
@@ -1,19 +1,5 @@
 import jwt from "express-jwt";
 
-const useJWT = jwt({
-  secret: process.env.JWT_SIGNATURE,
-  credentialsRequired: false,
-  getToken: req => {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.split(" ")[0] === "Bearer"
-    ) {
-      return req.headers.authorization.split(" ")[1];
-    }
-    return null;
-  }
-});
-
 const handleInvalidJWT = (err, req, res, next) => {
   if (err.code === "invalid_token") {
     res.status(401);
@@ -28,9 +14,18 @@ const handleInvalidJWT = (err, req, res, next) => {
 };
 
 // Protected routes can only be accessed by logged in users
-const requiresAuthentication = (req, res, next) => {
-  if (!req.user) return res.sendStatus(403);
-  next();
-};
+const requiresAuthentication = jwt({
+  secret: process.env.JWT_SIGNATURE,
+  credentialsRequired: false,
+  getToken: req => {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+      return req.headers.authorization.split(" ")[1];
+    }
+    return null;
+  }
+});
 
-export { useJWT, requiresAuthentication, handleInvalidJWT };
+export { requiresAuthentication, handleInvalidJWT };
--- src/server/api/routes.js
+++ src/server/api/routes.js
@@ -5,7 +5,7 @@ import nodeRouter from "./node";
 import documentRouter from "./document";
 import userRouter from "./user";
 // Middleware
-import { useJWT, handleInvalidJWT } from "./middleware";
+import { handleInvalidJWT } from "./middleware";
 
 //$ Router Setup
 const router = express({
@@ -17,9 +17,6 @@ const router = express({
 router.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({ extended: true }));
-// jwt for auth
-router.use(useJWT);
-router.use(handleInvalidJWT);
 
 //$ Health check the API
 router.get("/", (req, res) => res.sendStatus(200));
@@ -28,6 +25,9 @@ router.get("/", (req, res) => res.sendStatus(200));
 router.use("/node", nodeRouter);
 router.use("/document", documentRouter);
 router.use("/user", userRouter);
+
+//$ Attach middleware to handle expired JWT token errors
+router.use(handleInvalidJWT);
 
 // Export
 export default router;
