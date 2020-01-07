a548b01e1e46308c299d36e47fc21268405d216f
William A. Holley IV <will@vivism.com>
2020-01-07 12:52:28 -0500

    Adds alt colors to styled themes

--- src/client/styles/styledThemes.js
+++ src/client/styles/styledThemes.js
@@ -1,14 +1,39 @@
-const makeTheme = ({ textColor, background }) => ({
+const white = "white";
+const black = "#222";
+const blackGradient = "linear-gradient(45deg,#040404 0%,#020202 100%)";
+const actionBlue = "#2997ff";
+
+const makeTheme = ({
+  textColor,
+  background,
+  altBackground,
+  altText,
+  actionColor
+}) => ({
   text: {
-    color: textColor
+    color: textColor,
+    alt: altText
   },
-  background: background
+  background,
+  altBackground,
+  colors: {
+    action: actionColor
+  }
 });
 
 const dark = makeTheme({
-  textColor: "white",
-  background: "linear-gradient(45deg,#040404 0%,#020202 100%)"
+  textColor: white,
+  background: blackGradient,
+  altBackground: white,
+  altText: black,
+  actionColor: actionBlue
 });
-const light = makeTheme({ textColor: "#222", background: "white" });
+const light = makeTheme({
+  textColor: black,
+  background: white,
+  altBackground: blackGradient,
+  altText: white,
+  actionColor: actionBlue
+});
 
 export { dark, light };
