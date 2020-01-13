fb3ef09d36242070b29e6c46a7c26d38547530b6
William A. Holley IV <will@vivism.com>
2020-01-13 12:58:28 -0500

    adds postgraphile

--- src/server/graphql/postgraphile.js
+++ src/server/graphql/postgraphile.js
@@ -1,46 +1,14 @@
-/**
- * Option Documentation:
- * https://www.graphile.org/postgraphile/usage-library/
- */
-
 //* Libs
 import { postgraphile } from "postgraphile";
 //* Config
-import { connectionString } from "./../data/db";
-//* Postgraphile Plugins
-import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
+import { postgraphileConnection } from "./../data/db";
+import options from "./options";
 
-const commonOptions = {
-  subscriptions: true,
-  dynamicJson: true,
-  ignoreRBAC: false,
-  ignoreIndexes: false,
-  setofFunctionsContainNulls: false,
-  enableQueryBatching: true,
-  legacyRelations: "omit",
-  jwtSecret: process.env.JWT_SIGNATURE,
-  appendPlugins: [PgSimplifyInflectorPlugin]
-};
-const devOptions = {
-  watchPg: true,
-  showErrorStack: "json",
-  extendedErrors: ["hint", "detail", "errcode"],
-  exportGqlSchemaPath: "schema.graphql",
-  graphiql: true,
-  allowExplain: true,
-  ...commonOptions
-};
-const prodOptions = {
-  retryOnInitFail: true,
-  extendedErrors: ["errcode"],
-  graphiql: false,
-  // TODO: The default Postgraphile logging has performance issues, but do make sure you have a logging system in place!
-  disableQueryLog: true,
-  ...commonOptions
-};
+/**
+ * Created async
+ */
+function createPGQLClient() {
+  return postgraphile(postgraphileConnection, ["public", "document"], options);
+}
 
-export default postgraphile(
-  connectionString,
-  ["public", "document"], // schemas to expose
-  process.env.NODE_ENV === "development" ? devOptions : prodOptions
-);
+export default createPGQLClient;
