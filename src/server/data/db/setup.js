07117fdd4195936912c06a81b97c598e608c8617
William A. Holley IV <will@vivism.com>
2020-01-14 17:13:51 -0500

    node/edge insertion

--- src/server/data/db/setup.js
+++ src/server/data/db/setup.js
@@ -106,7 +106,28 @@ const functions = [
     CREATE FUNCTION public.current_user_id() RETURNS uuid AS $$
       SELECT NULLIF(current_setting('jwt.claims.user_id', true), '')::uuid;
     $$ LANGUAGE sql STABLE;
+  `,
+  // Set document created by
   `
+  DROP FUNCTION IF EXISTS document.on_insert_document CASCADE;
+  CREATE FUNCTION document.on_insert_document() RETURNS trigger AS $$
+  BEGIN
+    -- Validate that a user id is present
+    IF current_user_id() IS NULL THEN
+      RAISE EXCEPTION 'current_user_id is null!';
+    END IF;
+    -- Update
+    NEW.created_by := current_user_id();
+    RETURN NEW;
+  END;
+  $$ LANGUAGE 'plpgsql';
+  `
+];
+
+const triggers = [
+  // When a document is created, set the created_by value.
+  `CREATE TRIGGER set_created_by BEFORE INSERT ON ${DOCUMENT_REF}
+  FOR EACH ROW EXECUTE FUNCTION document.on_insert_document();`
 ];
 
 const roles = [
@@ -118,6 +139,7 @@ const roles = [
   BEGIN
     SELECT 1 INTO count FROM pg_roles WHERE rolname = '${gqlUser}';
   IF count > 0 THEN
+    DROP OWNED BY ${gqlUser};
     REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM ${gqlUser};
     REVOKE ALL ON SCHEMA document FROM ${gqlUser};
     DROP ROLE ${gqlUser};
@@ -134,6 +156,7 @@ const roles = [
   BEGIN
     SELECT 1 INTO count FROM pg_roles WHERE rolname = 'app_user';
   IF count > 0 THEN
+    DROP OWNED BY app_user;
     REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM app_user;
     REVOKE ALL ON SCHEMA document FROM app_user;
     DROP ROLE app_user;
@@ -144,20 +167,20 @@ const roles = [
 
 /**
  ** Public Permissions
- * - [x] execute public.register_user
- * - [x] execute public.login_user
- * - [x] select public documents
- * - [x] select nodes/edges within public documents
+ * - execute public.register_user
+ * - execute public.login_user
+ * - select public documents
+ * - select nodes/edges within public documents
  ** User permissions (user inherits all public)
- * TODO: explicit inheritance rather than dual grants/assignments
- * - [x] app_user can select any public document/node/edge
+ * TODO: explicit inheritance rather than dual grants/assignments?
+ * - app_user can select any public document/node/edge
  * - document/node/edge tables
- * -- [x] app_user can insert on document table
- * -- [x] app_user can read select/update documents they've authored
- * -- [] app_user can select/insert/update nodes on documents they've authored
- * -- [] app_user can select/insert/update edges on documents they've authored
- * - [x] app_user can query their own user record
- * - [x] set current user id when bearer token is present
+ * -- app_user can insert on document table
+ * -- app_user can read select/update documents they've authored
+ * -- app_user can select/insert/update nodes on documents they've authored
+ * -- app_user can select/insert/update edges on documents they've authored
+ * - app_user can query their own user record
+ * - set current user id when bearer token is present
  */
 const permissions = [
   /**
@@ -227,7 +250,7 @@ const permissions = [
   AS PERMISSIVE FOR SELECT TO app_user
   USING ("created_by" = current_user_id());`,
   // select node
-  `CREATE POLICY private_visibility ON ${DOCUMENT_REF}
+  `CREATE POLICY private_visibility ON ${NODE_REF}
   AS PERMISSIVE FOR SELECT TO app_user
   USING (
     EXISTS (
@@ -245,29 +268,87 @@ const permissions = [
       SELECT ${DOCUMENT_REF}.id
       FROM ${NODE_REF}
       JOIN ${DOCUMENT_REF} ON ${DOCUMENT_REF}.id = ${NODE_REF}.document
-      WHERE 
+      WHERE
         ${DOCUMENT_REF}.created_by = current_user_id() AND (
           ${NODE_REF}.id = node_a OR
           ${NODE_REF}.id = node_b
         )
     )
   );`,
-  // user can update/insert
-  `GRANT UPDATE, INSERT ON TABLE ${DOCUMENT_REF} TO app_user;`,
-  `GRANT UPDATE, INSERT ON TABLE ${NODE_REF} TO app_user;`,
-  `GRANT UPDATE, INSERT ON TABLE ${EDGE_REF} TO app_user;`,
+  //* User can insert / update documents
+  `GRANT TRIGGER, UPDATE, INSERT ON TABLE ${DOCUMENT_REF} TO app_user;`,
+  `GRANT EXECUTE ON FUNCTION document.on_insert_document TO app_user;`,
   // row level policies
-  // document update/insert
+  // document insert
+  `CREATE POLICY app_user_insert ON ${DOCUMENT_REF}
+  AS PERMISSIVE FOR INSERT TO app_user
+  WITH CHECK (TRUE);`,
+  // document update
   `CREATE POLICY creator_update ON ${DOCUMENT_REF}
   AS PERMISSIVE FOR UPDATE TO app_user
   USING ("created_by" = current_user_id());`,
-  `CREATE POLICY app_user_insert ON ${DOCUMENT_REF}
+  //* User can insert / update nodes
+  `GRANT UPDATE, INSERT ON TABLE ${NODE_REF} TO app_user;`,
+  // node insert
+  `CREATE POLICY app_user_insert ON ${NODE_REF}
   AS PERMISSIVE FOR INSERT TO app_user
-  WITH CHECK (TRUE);`
-  // TODO: add a trigger to automatically set createdBy on document,
-  // TODO and do not allow it to be set otherwise
-  // TODO node update/insert
-  // TODO edge update/insert
+  WITH CHECK (
+    EXISTS (
+      SELECT private
+      FROM ${DOCUMENT_REF}
+      WHERE id = document AND document.created_by = current_user_id()
+    )
+  );`,
+  // node update
+  `CREATE POLICY creator_update ON ${NODE_REF}
+  AS PERMISSIVE FOR UPDATE TO app_user
+  USING (
+    EXISTS (
+      SELECT private
+      FROM ${DOCUMENT_REF}
+      WHERE id = document AND document.created_by = current_user_id() 
+    )
+  );`,
+  //* User can insert / update edges
+  `GRANT UPDATE, INSERT ON TABLE ${EDGE_REF} TO app_user;`,
+  // edge insert
+  `CREATE POLICY app_user_insert ON ${EDGE_REF}
+  AS PERMISSIVE FOR INSERT TO app_user
+  WITH CHECK (
+    -- node_a's document is owned by user
+    EXISTS (
+      SELECT ${DOCUMENT_REF}.id
+      FROM ${NODE_REF}
+      JOIN ${DOCUMENT_REF} ON ${DOCUMENT_REF}.id = ${NODE_REF}.document
+      WHERE ${DOCUMENT_REF}.created_by = current_user_id() AND ${NODE_REF}.id = node_a
+    ) AND
+    -- node_b's document is owned by user
+    EXISTS (
+      SELECT ${DOCUMENT_REF}.id
+      FROM ${NODE_REF}
+      JOIN ${DOCUMENT_REF} ON ${DOCUMENT_REF}.id = ${NODE_REF}.document
+      WHERE ${DOCUMENT_REF}.created_by = current_user_id() AND ${NODE_REF}.id = node_b
+    )
+  );`,
+  // edge update
+  `CREATE POLICY creator_update ON ${EDGE_REF}
+  AS PERMISSIVE FOR UPDATE TO app_user
+  USING (
+    -- node_a's document is owned by user
+    EXISTS (
+      SELECT ${DOCUMENT_REF}.id
+      FROM ${NODE_REF}
+      JOIN ${DOCUMENT_REF} ON ${DOCUMENT_REF}.id = ${NODE_REF}.document
+      WHERE ${DOCUMENT_REF}.created_by = current_user_id() AND ${NODE_REF}.id = node_a
+    ) AND
+    -- node_b's document is owned by user
+    EXISTS (
+      SELECT ${DOCUMENT_REF}.id
+      FROM ${NODE_REF}
+      JOIN ${DOCUMENT_REF} ON ${DOCUMENT_REF}.id = ${NODE_REF}.document
+      WHERE document.document.created_by = current_user_id() AND ${NODE_REF}.id = node_b
+    )
+  );`
 ];
 
 const schemas = ["document"];
@@ -300,6 +381,7 @@ async function _execQuery(query) {
 const createTypes = async () => _execQuery(types.join(""));
 const createExtensions = async () => _execQuery(extensions.join());
 const createFunctions = async () => _execQuery(functions.join(""));
+const createTriggers = async () => _execQuery(triggers.join(""));
 const createRoles = async () => _execQuery(roles.join(""));
 const createPermissions = async () => _execQuery(permissions.join(""));
 const createSchemas = async () =>
@@ -314,6 +396,8 @@ const createSchemas = async () =>
  * https://postgresconf.org/system/events/document/000/000/996/pgconf_us_2019.pdf -- pg27+
  * TODO: don't consider the above... DO IT B/C THE AUTHORS ADMIT THE DOCS SUCK
  * https://github.com/graphile/postgraphile/issues/1049
+ * TODO: this should fix issues where methods are visible to the unauthenticated graphql
+ * TODO: api because of the app/app_user grant.
  */
 async function setupDatabase(forceCreateTables = false) {
   // Pre-table creation
@@ -328,6 +412,7 @@ async function setupDatabase(forceCreateTables = false) {
   // Post-table creation
   await createRoles();
   await createFunctions();
+  await createTriggers();
   await createPermissions();
 }
 
