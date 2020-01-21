c3f1999517c6e647edd10b7d82b2519c41727f0e
William A. Holley IV <will@vivism.com>
2020-01-21 15:50:39 -0500

    Enable escape hotkey

--- src/client/views/Document/hotkeys/escape.js
+++ src/client/views/Document/hotkeys/escape.js
@@ -1,16 +1,11 @@
-//* libraries
-import { useDispatch } from "react-redux";
 //* hooks
 import { useHotkeys } from "client/utils/hooks";
-//* actions
-import { setActiveNode } from "client/data/services/document/actions";
 
 /**
  * Deactivate all nodes
  */
-const useEscape = () => {
-  const dispatch = useDispatch();
-  const handler = () => dispatch(setActiveNode(null));
+const useEscape = setActiveNodeId => {
+  const handler = event => setActiveNodeId(null);
   useHotkeys("Escape", handler);
 };
 
