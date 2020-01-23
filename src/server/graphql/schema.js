c7288cd38930f62d018b9ea0c72499359338d225
William A. Holley IV <will@vivism.com>
2020-01-22 22:52:29 -0500

    fixes edges resolution

--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -1,4 +1,5 @@
 import { makeExtendSchemaPlugin, gql, embed } from "graphile-utils";
+import v from "voca";
 
 export default makeExtendSchemaPlugin(({ pgSql: sql, inflection }) => ({
   typeDefs: gql`
@@ -25,7 +26,8 @@ export default makeExtendSchemaPlugin(({ pgSql: sql, inflection }) => ({
       ) => {
         return jwtClaims ? createdBy === jwtClaims.user_id : null;
       },
-      edges: async ({ id }, args, { pgClient }, resolveInfo) => {
+      edges: async ({ id }, args, { pgClient }, { graphile }) => {
+        // Is this a hack?  Is now the time for caring... no.
         const { rows } = await pgClient.query(`
           SELECT * FROM document.edge WHERE id IN (
             SELECT DISTINCT(document.edge.id)
@@ -33,7 +35,15 @@ export default makeExtendSchemaPlugin(({ pgSql: sql, inflection }) => ({
             JOIN document.node ON document.node.id = document.edge.node_a OR document.node.id = document.edge.node_b
             WHERE document.node.document = '${id}'
           );`);
-        return rows;
+        // Convert the key casing
+        const camelRows = rows.map(row => {
+          const copy = {};
+          Object.keys(row).forEach(key => {
+            copy[v.camelCase(key)] = row[key];
+          });
+          return copy;
+        });
+        return camelRows;
       }
     }
   }
