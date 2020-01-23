240a0599db35a21a2e90ca0b3140b200fc0a731d
William A. Holley IV <will@vivism.com>
2020-01-22 22:30:52 -0500

    adds document.edges

--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -12,6 +12,7 @@ export default makeExtendSchemaPlugin(({ pgSql: sql, inflection }) => ({
           );
         })}
       )
+      edges: [Edge]! @requires(columns: ["id"])
     }
   `,
   resolvers: {
@@ -23,6 +24,16 @@ export default makeExtendSchemaPlugin(({ pgSql: sql, inflection }) => ({
         resolveInfo
       ) => {
         return jwtClaims ? createdBy === jwtClaims.user_id : null;
+      },
+      edges: async ({ id }, args, { pgClient }, resolveInfo) => {
+        const { rows } = await pgClient.query(`
+          SELECT * FROM document.edge WHERE id IN (
+            SELECT DISTINCT(document.edge.id)
+            FROM document.edge
+            JOIN document.node ON document.node.id = document.edge.node_a OR document.node.id = document.edge.node_b
+            WHERE document.node.document = '${id}'
+          );`);
+        return rows;
       }
     }
   }
